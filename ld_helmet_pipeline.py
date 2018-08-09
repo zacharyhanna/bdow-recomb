a3e8ccda48deaf2d48db8cfd062be412221aafba
ZRH <hcaz@zacharys-air.calacademy.org>
2018-08-08 18:45:32 -0700

    Update ld_helmet_pipeline and pipeline notes.

--- ld_helmet_pipeline.py
+++ ld_helmet_pipeline.py
@@ -3,7 +3,7 @@ import sys
 import glob
 """
 Example run:
-python ld_helmet_pipeline.py <run || test> scaffold_list_file
+python ld_helmet_pipeline.py <run || test> scaffold_list_file theta_list_file
 """
 
 def Run(command): #run the command in the bash shell
@@ -34,14 +34,19 @@ def get_scafs(file_in):
 
 def rename_sample_seq(fasta, samp, haplo, new_fasta):
     cmd = "bioawk -v mysamp=" + samp + " -v myhaplo=" + haplo + \
-    " -c fastx \'{print \">\"$name\"_$\{mysamp\}_$\{myhaplo\}\n\"$seq}\'" + " " \
+    " -c fastx \'{print \">\"$name\"_\"mysamp\"_\"myhaplo\"\\n\"$seq}\'" + " " \
     + fasta + " >" + new_fasta
-    Run(cmd)
+    #Run(cmd)
+    # remove old fasta
+    rm_fasta_cmd = "rm " + fasta
+    #Run(rm_fasta_cmd)
 
 def rename_fastas(scaf):
     fasta_dir = "/media/walllab/zhanna/owl/ldhelmet/"
     fasta_pattern = fasta_dir + "*_" + scaf + ":*.fasta"
     fasta_ls = glob.glob(fasta_pattern)
+    new_fasta_ls = []
+    combo_fasta = fasta_dir + scaf + "/" + scaf + "_BADOeast_haplos.fasta"
     for fasta in fasta_ls:
         # split full file path
         splitfasta = fasta.split("/")
@@ -56,11 +61,17 @@ def rename_fastas(scaf):
         haplo = haplosidesplit[0]
         new_fasta_dir = fasta_dir + scaf
         new_fasta = fasta_dir + scaf + "/" + scaf + "_" + samp + "_" + haplo + ".fasta"
+        new_fasta_ls.append(new_fasta)
         mkdir_cmd = "mkdir -p " + new_fasta_dir
-        Run(mkdir_cmd)
+        #Run(mkdir_cmd)
         rename_sample_seq(fasta, samp, haplo, new_fasta)
-
-    pass
+    combine_fastas_cmd = "cat " + fasta_dir + scaf + "/*.fasta >" + combo_fasta
+    #Run(combine_fastas_cmd)
+    # remove the individual sample fastas after combining them into the total fasta
+    for mynew_fasta in new_fasta_ls:
+        rm_fasta_cmd = "rm " + mynew_fasta
+        #Run(rm_fasta_cmd)
+    return combo_fasta
 
 def vcf2fasta(scaf):
     reference_dir = "/media/walllab/zhanna/owl/assemblies/StrOccCau2_scafs/"
@@ -73,12 +84,42 @@ def vcf2fasta(scaf):
     #cat output/{params.region}/{params.numSeq}sequences/chr{params.chromosome}/individualFastaFiles/* > {output}
     #rm -rf output/{params.region}/{params.numSeq}sequences/chr{params.chromosome}/individualFastaFiles/
 
+def ldhelmet_find_confs(scaf, combined_fasta):
+    input = combined_fasta
+    output_base = combined_fasta[:-5]
+    output = output_base + "conf"
+    find_confs_cmd = "~/bin/LDhelmet_v1.10/ldhelmet find_confs --num_threads 12 -w 50 -o " + output + " " + input
+    Run(find_confs_cmd)
+
+def get_thetas(file_in):
+    theta_dict = {}
+    with open(file_in, 'r') as infile:
+        for line in infile:
+            splitline = line.strip().split()
+            # first column is the scaffold
+            # second column is the theta value
+            theta_dict[splitline[0]] = str(splitline[1])
+    return theta_dict
+
+def ldpop(scaf, combined_fasta, scaf_theta):
+    activate_ldpop = "source activate LDpopPy3"
+    deactivate_ldpop = "source deactivate"
+    Run(activate_ldpop)
+    output = combined_fasta[:-5] + "ldpop"
+    ldpop_cmd = "~/bin/ldpop/run/ldtable.py -n 24 -th " + scaf_theta + " -rh 101,100 --approx --cores 12 >" + output
+    Run(ldpop_cmd)
+    Run(deactivate_ldpop)
+
 def run_pipeline(scaf_file):
     scaf_ls = get_scafs(scaf_file)
+    theta_dict = get_thetas(sys.argv[3])
     for scaf in scaf_ls:
+        scaf_theta = theta_dict[scaf]
         #get_vcf_data(scaf)
         #vcf2fasta(scaf)
-        rename_fastas(scaf)
+        combo_fasta = rename_fastas(scaf)
+        #ldhelmet_find_confs(scaf, combo_fasta)
+        ldpop(scaf, combo_fasta, scaf_theta)
 
 
 if sys.argv[1] == "run":
